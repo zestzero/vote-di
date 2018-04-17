628234f52dd2ab3dbaff0ff69a1c1a2f6e1858b5
Krit B <Krit.b@taskworld.com>
2018-04-17 16:49:19 +0700

    - Eject create-react-app and reconfigure scss loader. - Add firebase initializer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import * as firebaseui from 'firebaseui';
+import * as firebase from 'firebase';
+import { firebaseInitializer } from './services/firebase-services';
 class App extends Component {
+  constructor () {
+    super();
+    // const ui = new firebaseui.auth.AuthUI(firebase.auth());
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Vote-Di</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          An application that allow you to create your own poll for helping you to decide things from your friends.
         </p>
+        <div id="firebaseui-auth-container"></div>
+        <div id="loader">Loading...</div>
       </div>
     );
   }
